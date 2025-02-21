8292a7d36f5cae14c7e7a780cda8c7cf65509c21
byusufa <jozef12079104293@gmail.com>
2025-02-21 07:33:38 +0500

    init commit

---
+++ src/main/java/uz/pdp/githubinitdemo/GithubInitDemoApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.githubinitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubInitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubInitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/githubinitdemo/GithubInitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.githubinitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubInitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
